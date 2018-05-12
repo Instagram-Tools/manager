5c157706b056653cbd9976700d206ad8882bd044
Gott50 <xxGott50xx@gmail.com>
2018-05-12 09:49:18 +0200

    extract update_user(data)

--- manager/manager.py
+++ manager/manager.py
@@ -14,9 +14,8 @@ from models import *
 @app.route('/', methods=['POST'])
 def index():
     try:
-        d = json.loads(request.data)
-        user = User(username=d.get("username"), password=d.get("password"),
-                    settings=d.get("settings"), timetable=d.get("timetable"))
+        data = json.loads(request.data)
+        user = update_user(data)
 
         first = User.query.filter_by(username=user.username).first()
         if first:
@@ -32,6 +31,24 @@ def index():
         return str(User.query.all())
     except Exception as exc:
         return str(exc)
+
+
+def update_user(data):
+    user = User(username=data.get("username"), password=data.get("password"),
+                settings=data.get("settings"))
+    first = User.query.filter_by(username=user.username).first()
+    if first:
+        first.password = user.password
+        first.settings = user.settings
+
+        for t in first.timetables:
+            db.session.delete(t)
+
+        db.session.add(first)
+        user = first
+    db.session.add(user)
+    db.session.commit()
+    return user
 
 
 if __name__ == '__main__':
