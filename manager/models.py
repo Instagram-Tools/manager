862389c39d515d6b1b2313de20711d1751769fe5
Gott50 <xxGott50xx@gmail.com>
2018-05-12 09:49:02 +0200

    update DB Schema

--- manager/models.py
+++ manager/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     settings = db.Column(db.Text(), nullable=False)
-    timetable = db.relationship('TimeTable', backref='user', lazy=True)
+    timetables = db.relationship('TimeTable', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -21,3 +21,6 @@ class TimeTable(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     start = db.Column(db.DateTime(), nullable=False)
     end = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return '<TimeTable %r %r:%r>' % (self.user_id, str(self.start), str(self.end))
