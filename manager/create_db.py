e4fb9db3253c5783e0221202cce93e2582fbf421
Gott50 <xxGott50xx@gmail.com>
2018-05-12 09:04:23 +0200

    update DB Schema

--- manager/create_db.py
+++ manager/create_db.py
@@ -1,3 +1,7 @@
 from manager import db
 
+# Drop all of the existing database tables
+db.drop_all()
+
+# Create the database and the database table
 db.create_all()
--- manager/models.py
+++ manager/models.py
@@ -2,14 +2,22 @@ from manager import db
 
 
 class User(db.Model):
-
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     settings = db.Column(db.Text(), nullable=False)
-    timetable = db.Column(db.Text(), nullable=False)
+    timetable = db.relationship('TimeTable', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class TimeTable(db.Model):
+    __tablename__ = 'timetable'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start = db.Column(db.DateTime(), nullable=False)
+    end = db.Column(db.DateTime(), nullable=False)
