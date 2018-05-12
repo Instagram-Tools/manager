80b378acabd954a6810522c3299f498ca628ca45
Gott50 <xxGott50xx@gmail.com>
2018-05-12 11:56:27 +0200

    add update_timetable(user, data)

--- manager/manager.py
+++ manager/manager.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 import json
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
@@ -16,15 +17,7 @@ def index():
     try:
         data = json.loads(request.data)
         user = update_user(data)
-
-        first = User.query.filter_by(username=user.username).first()
-        if first:
-            first.password = user.password
-            first.settings = user.settings
-            first.timetable = user.timetable
-            db.session.add(first)
-        else:
-            db.session.add(user)
+        update_timetable(user, data)
 
         db.session.commit()
 
@@ -49,6 +42,32 @@ def update_user(data):
     db.session.add(user)
     db.session.commit()
     return user
+
+
+def update_timetable(user, data):
+    timetable = data.get("timetable", [])
+    for i in range(0, len(timetable), 2):
+        timetable = TimeTable(user_id=user.id, start=parse_datetime(timetable[i]),
+                              end=parse_datetime(timetable[i + 1]))
+        db.session.add(timetable)
+    db.session.commit()
+
+
+def parse_datetime(line):
+    p = parse_datetime_prefix(str(line), '%Y-%m-%d %H:%M:%S')
+    return datetime.datetime(1, 1, day=p.isoweekday(), hour=p.hour, minute=p.minute, second=p.second)
+
+
+def parse_datetime_prefix(line, fmt):
+    try:
+        t = datetime.datetime.strptime(line, fmt)
+    except ValueError as v:
+        if len(v.args) > 0 and v.args[0].startswith('unconverted data remains: '):
+            line = line[:-(len(v.args[0]) - 26)]
+            t = datetime.datetime.strptime(line, fmt)
+        else:
+            raise
+    return t
 
 
 if __name__ == '__main__':
