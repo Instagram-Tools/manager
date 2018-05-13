e68a3c3c734b1af559ffc18f919753bfd6716774
Gott50 <xxGott50xx@gmail.com>
2018-05-13 09:22:13 +0200

    add sleep()

--- manager/time_util.py
+++ manager/time_util.py
@@ -1,5 +1,46 @@
 """Helper module to handle time related stuff"""
 import datetime
+from random import gauss
+from time import sleep as original_sleep
+
+# Amount of variance to be introduced
+# i.e. random time will be in the range: TIME +/- STDEV %
+STDEV = 0.5
+sleep_percentage = 1
+
+
+def randomize_time(mean):
+    allowed_range = mean * STDEV
+    stdev = allowed_range / 3  # 99.73% chance to be in the allowed range
+
+    t = 0
+    while abs(mean - t) > allowed_range:
+        t = gauss(mean, stdev)
+
+    return t
+
+
+def set_sleep_percentage(percentage):
+    global sleep_percentage
+    sleep_percentage = percentage / 100
+
+
+def sleep(t, custom_percentage=None):
+    try:
+        if custom_percentage is None:
+            custom_percentage = sleep_percentage
+        time = randomize_time(t) * custom_percentage
+        print("sleep: " + str(time))
+        original_sleep(time)
+        return True
+
+    except Exception as exc:
+        print(exc)
+        return False
+
+
+def sleep_actual(t):
+    original_sleep(t)
 
 
 def parse_datetime(line):
