8727da19f037618ed04033fcc8f3701787238f49
Gott50 <xxGott50xx@gmail.com>
2018-05-13 09:17:29 +0200

    add Model Running to DB Schema

--- manager/models.py
+++ manager/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     password = db.Column(db.String(80), nullable=False)
     settings = db.Column(db.Text(), nullable=False)
     timetables = db.relationship('TimeTable', backref='user', lazy=True)
+    running = db.relationship('Running', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -24,3 +25,15 @@ class TimeTable(db.Model):
 
     def __repr__(self):
         return '<TimeTable %r %r:%r>' % (self.user_id, str(self.start), str(self.end))
+
+
+class Running(db.Model):
+    __tablename__ = 'running'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start = db.Column(db.DateTime(), nullable=False)
+    end = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return '<Running %r %r:%r>' % (self.user_id, str(self.start), str(self.end))
