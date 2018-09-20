a5c40abd46283c7c099d026ea6c493967850469c
Gott50 <xxGott50xx@gmail.com>
2018-09-20 20:23:07 +0200

    add print("db.create_all()")

--- src/create_db.py
+++ src/create_db.py
@@ -1,7 +1,9 @@
 from server import db
 
 # Drop all of the existing database tables
+# print("db.drop_all()")
 # db.drop_all()
 
 # Create the database and the database table
+print("db.create_all()")
 db.create_all()
