c7b8aa05f32f64c63a0d30e39bc917712fda5556
Gott50 <xxGott50xx@gmail.com>
2018-09-30 13:30:04 +0200

    add sudo Docker commands

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -9,10 +9,10 @@ rm ./tmp
 
 ssh -tt -i ./id_rsa docker@$IP <<-"END_SSH"
 
-    docker stop /$1
-    docker rm /$1
+    sudo docker stop /$1
+    sudo docker rm /$1
 
-    docker run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 \
+    sudo docker run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 \
     -e ENV=$3 -v log_data:/code/logs instagramtools/web
 
 END_SSH
