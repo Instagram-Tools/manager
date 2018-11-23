3bc4f9bb24574974855d368c4bcdeaf276a68c99
Gott50 <xxGott50xx@gmail.com>
2018-11-23 20:29:43 +0100

    add stop_bot.sh

---
+++ src/stop_bot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir ~/.ssh
+echo $KNOWN_HOSTS > ~/.ssh/known_hosts
+
+echo $SSH_KEY > ./tmp
+tr '_' '\n' < ./tmp > ./id_rsa
+chmod 600 ./id_rsa
+rm ./tmp
+
+CMD="sudo docker stop /$1"
+# echo CMD: $CMD
+
+ssh -tt -i ./id_rsa $P_USER@$IP $CMD
