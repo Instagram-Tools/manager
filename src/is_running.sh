abbc245684e25bfc3bf1e4d9ec811892d8e1d38d
Gott50 <xxGott50xx@gmail.com>
2018-11-23 16:06:17 +0100

    add is_running.sh

---
+++ src/is_running.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir ~/.ssh
+echo $KNOWN_HOSTS > ~/.ssh/known_hosts
+
+echo $SSH_KEY > ./tmp
+tr '_' '\n' < ./tmp > ./id_rsa
+chmod 600 ./id_rsa
+rm ./tmp
+
+CMD="sudo docker ps -f NAME=$1 | grep -w $1"
+# echo CMD: $CMD
+
+ssh -tt -i ./id_rsa $P_USER@$IP $CMD
