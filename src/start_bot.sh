8429cfb4fd1a6fef26dce357f33ff70341d55309
Gott50 <xxGott50xx@gmail.com>
2018-11-28 11:29:41 +0100

    Revert: do not convert STR=$1

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -7,9 +7,16 @@ tr '_' '\n' < ./tmp > ./id_rsa
 chmod 600 ./id_rsa
 rm ./tmp
 
+STR=$1
+JSON=${STR//'"'/'\"'}
+JSON2=${JSON//'{'/'\{'}
+JSON3=${JSON2//'}'/'\}'}
+shift
+
+echo Manager JSON: $JSON3
 echo Manager Parameters: $@
 
-CMD="bash start_bot.sh $@"
+CMD="bash start_bot.sh $JSON3 $@"
 echo CMD: $CMD
 
-ssh -tt -i ./id_rsa $P_USER@$IP bash start_bot.sh $@
+ssh -tt -i ./id_rsa $P_USER@$IP $CMD
