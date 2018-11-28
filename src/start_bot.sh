749c7a652cee61225c415edb5d7932b4ca681142
Gott50 <xxGott50xx@gmail.com>
2018-11-28 09:41:20 +0100

    do not convert STR=$1

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -7,16 +7,9 @@ tr '_' '\n' < ./tmp > ./id_rsa
 chmod 600 ./id_rsa
 rm ./tmp
 
-STR=$1
-JSON=${STR//'"'/'\"'}
-JSON2=${JSON//'{'/'\{'}
-JSON3=${JSON2//'}'/'\}'}
-shift
-
-echo Manager JSON: $JSON3
 echo Manager Parameters: $@
 
-CMD="bash start_bot.sh $JSON3 $@"
+CMD="bash start_bot.sh $@"
 echo CMD: $CMD
 
-ssh -tt -i ./id_rsa $P_USER@$IP $CMD
+ssh -tt -i ./id_rsa $P_USER@$IP bash start_bot.sh $@
