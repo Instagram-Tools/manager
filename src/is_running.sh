7ef7695144aad55b6d9ac21b5b1a32ef0337afd1
Gott50 <xxGott50xx@gmail.com>
2018-12-03 07:25:37 +0100

    do not use -a for "docker ps"

--- src/is_running.sh
+++ src/is_running.sh
@@ -6,7 +6,7 @@ tr '_' '\n' < ./tmp > ./id_rsa
 chmod 600 ./id_rsa
 rm ./tmp
 
-CMD="sudo docker ps -a --format 'table {{.Names}}' -f STATUS=running -f NAME=$1 | grep -w $1"
+CMD="sudo docker ps --format 'table {{.Names}}' -f STATUS=running -f NAME=$1 | grep -w $1"
 # echo CMD: $CMD
 
 ssh -tt -i ./id_rsa $P_USER@$IP $CMD
