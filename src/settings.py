8c4defc87e7cace8e4793636f086f83a031041b4
Gott50 <xxGott50xx@gmail.com>
2018-12-03 06:19:59 +0100

    optimize Imports

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
-from manager import Manager
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
