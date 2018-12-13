3ef66072d83a3c233ec20b78c722c84a89a9a67d
Gott50 <xxGott50xx@gmail.com>
2018-12-14 00:51:45 +0100

    fix File not found

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -13,7 +13,7 @@ shift
 echo Manager JSON: $JSON
 echo Manager Parameters: $@
 
-CMD="bash start_bot.sh '$JSON' $@"
+CMD="bash start-bot.sh '$JSON' $@"
 echo Manager CMD: $CMD
 
-ssh -o StrictHostKeychecking=no -tt -i ./id_rsa $P_USER@$IP "bash start_bot.sh '$JSON' $@"
+ssh -o StrictHostKeychecking=no -tt -i ./id_rsa $P_USER@$IP "bash start-bot.sh '$JSON' $@"
