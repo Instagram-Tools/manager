eb3e1497289a9d4b8fa8f729fbc62b40dc937364
Gott50 <xxGott50xx@gmail.com>
2018-12-23 15:48:43 +0100

    fix scp in start_bot.sh

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -16,5 +16,5 @@ echo Manager Parameters: $@
 CMD="bash start_bot.sh '$JSON' $@"
 echo Manager CMD: $CMD
 
-scp -r ./bot_scripts/ $P_USER@$IP:
+scp -o StrictHostKeychecking=no -i ./id_rsa -r ./bot_scripts $P_USER@$IP:
 ssh -o StrictHostKeychecking=no -tt -i ./id_rsa $P_USER@$IP "bash bot_scripts/start_bot.sh '$JSON' $@"
