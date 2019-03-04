f49ace4a063418c159a80c23670b5a3df655bd48
Gott50 <xxGott50xx@gmail.com>
2019-03-04 22:16:02 +0100

    fix docker Error response from daemon: Invalid container name (_mr_bonvivant_), only [a-zA-Z0-9][a-zA-Z0-9_.-] are allowed.

--- src/bot_scripts/start_bot.sh
+++ src/bot_scripts/start_bot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo service docker start
+sudo service docker status
 
 sudo docker pull instagramtools/web
 
@@ -18,15 +19,15 @@ echo Composition Parameters: $@
 INSTA_USER=$2
 INSTA_PW=$3
 
-echo "Composition sudo docker stop /$INSTA_USER"
-sudo docker stop /$INSTA_USER
-echo "Composition sudo docker rm /$INSTA_USER"
-sudo docker rm /$INSTA_USER
+echo "Composition sudo docker stop /bot"
+sudo docker stop /bot
+echo "Composition sudo docker rm /bot"
+sudo docker rm /bot
 
 SETTINGS=${1//' '/''}
 
 
-CMD="sudo docker run -d -v /home/ec2-user/logs:/root/InstaPy/logs -v /home/ec2-user/db:/root/InstaPy/db --net=bridge --link selenium:selenium -e SELENIUM=selenium --name $INSTA_USER -e ENV=$SETTINGS -e INSTA_USER=$INSTA_USER -e INSTA_PW=$INSTA_PW instagramtools/web sh ./wait-for-selenium.sh http://selenium:4444/wd/hub -- python docker_quickstart.py"
+CMD="sudo docker run -d -v /home/ec2-user/logs:/root/InstaPy/logs -v /home/ec2-user/db:/root/InstaPy/db --net=bridge --link selenium:selenium -e SELENIUM=selenium --name bot -e ENV=$SETTINGS -e INSTA_USER=$INSTA_USER -e INSTA_PW=$INSTA_PW instagramtools/web sh ./wait-for-selenium.sh http://selenium:4444/wd/hub -- python docker_quickstart.py"
 echo Composition CMD: $CMD
 
 $CMD
