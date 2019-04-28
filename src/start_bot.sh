0b7580f73b344d4a6af87ed8207f4ca6c33d9de6
Gott50 <xxGott50xx@gmail.com>
2019-04-28 23:41:49 +0200

    remove extra Logging used for Debugging

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -10,11 +10,11 @@ shift
 JSON=$1
 shift
 
-echo Manager JSON: $JSON
-echo Manager Parameters: $@
+# echo Manager JSON: $JSON
+# echo Manager Parameters: $@
 
 CMD="bash start_bot.sh '$JSON' $@"
-echo Manager CMD: $CMD
+# echo Manager CMD: $CMD
 
 scp -o StrictHostKeychecking=no -i ./id_rsa -r ./bot_scripts $P_USER@$IP:
 ssh -o StrictHostKeychecking=no -tt -i ./id_rsa $P_USER@$IP "bash bot_scripts/start_bot.sh '$JSON' $@"
