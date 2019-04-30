d2a2647a28398050f48099dac26647c0b4a8a7e0
Gott50 <xxGott50xx@gmail.com>
2019-04-30 14:36:22 +0200

    add echo after Bot started

--- src/start_bot.sh
+++ src/start_bot.sh
@@ -10,6 +10,8 @@ shift
 JSON=$1
 shift
 
+INSTA_USER=$1
+
 # echo Manager JSON: $JSON
 # echo Manager Parameters: $@
 
@@ -18,3 +20,5 @@ CMD="bash start_bot.sh '$JSON' $@"
 
 scp -o StrictHostKeychecking=no -i ./id_rsa -r ./bot_scripts $P_USER@$IP:
 ssh -o StrictHostKeychecking=no -tt -i ./id_rsa $P_USER@$IP "bash bot_scripts/start_bot.sh '$JSON' $@"
+
+echo Manager started Bot: $INSTA_USER
