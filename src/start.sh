56b7b8163ea2fc3661162466f287348f50c434da
Gott50 <xxGott50xx@gmail.com>
2019-05-03 14:14:42 +0200

    move create id_rsa to start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo $SSH_KEY > ./tmp
+tr '_' '\n' < ./tmp > ./id_rsa
+chmod 600 ./id_rsa
+rm ./tmp
+
 # /usr/local/bin/gunicorn -b :$1 server:app & python start.py
 python start.py
--- src/start_bot.sh
+++ src/start_bot.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-echo $SSH_KEY > ./tmp
-tr '_' '\n' < ./tmp > ./id_rsa
-chmod 600 ./id_rsa
-rm ./tmp
 
 IP=$1
 shift
