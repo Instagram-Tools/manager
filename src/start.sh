c1070bd7f25487c6ccd4e18e1363bc845bdd64d6
Gott50 <xxGott50xx@gmail.com>
2019-05-21 09:13:56 +0200

    Revert "disable gunicorn server"
    This reverts commit 2347eb02

--- src/start.sh
+++ src/start.sh
@@ -5,5 +5,4 @@ tr '_' '\n' < ./tmp > ./id_rsa
 chmod 600 ./id_rsa
 rm ./tmp
 
-# /usr/local/bin/gunicorn -b :$1 server:app & python start.py
-python start.py
+/usr/local/bin/gunicorn -b :$1 server:app & python start.py
