4209253b655fcdfd1d1c7da2fe2220588c69d07d
Gott50 <xxGott50xx@gmail.com>
2019-05-25 01:49:48 +0200

    fix Exception: name 'account' is not defined

--- src/activity.py
+++ src/activity.py
@@ -87,7 +87,7 @@ class Activity:
             p.kill()
 
     def start_try_login(self, username, password, email):
-            ip = self.aws.start(user=account.username)
+            ip = self.aws.start(user=username)
             self.logger.warning("start_try_login for %s at ip: %s" % (username, ip))
 
             sleep(120)
