363a2e3e502c8ece90e90ee1ba2e0d947754de6c
Gott50 <xxGott50xx@gmail.com>
2019-05-25 02:00:11 +0200

    deal with gunicorn [CRITICAL] WORKER TIMEOUT

--- src/start.sh
+++ src/start.sh
@@ -5,6 +5,7 @@ tr '_' '\n' < ./tmp > ./id_rsa
 chmod 600 ./id_rsa
 rm ./tmp
 
-sleep 300
+echo sleep 30
+sleep 30
 
-/usr/local/bin/gunicorn -b :$1 server:app & python start.py
+/usr/local/bin/gunicorn --workers=10 --timeout 600 -b :$1 server:app & python start.py
